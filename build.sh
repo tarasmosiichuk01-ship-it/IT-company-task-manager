bfe8e4e155b6514fb22c2b8004b2d8dcabd139a7
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-03-27 14:59:17 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,5 +7,3 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
-
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_user('user', '', 'user12345') if not User.objects.filter(username='user').exists() else None"

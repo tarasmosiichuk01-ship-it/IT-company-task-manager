26920094328904905780d4b582b4ec7532e5d82d
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-03-27 14:53:04 +0200

    Add test user creation

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_user('user', '', 'user12345') if not User.objects.filter(username='user').exists() else None"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_user('user', '', 'user12345') if not User.objects.filter(username='user').exists() else None"
